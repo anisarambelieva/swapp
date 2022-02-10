4148bffae30d246a9fa245415e73a33c53b17bdf
Anna Sarambelieva <anisarambelieva@gmail.com>
2022-02-10 15:13:35 +0200

    Add more dummy characters

--- src/pages/characters.js
+++ src/pages/characters.js
@@ -4,6 +4,10 @@ import Character from "../components/character";
 import Header from "../components/header";
 
 import Annakin from "../assets/Annakin.webp"
+import Jabba from "../assets/Jabba.webp"
+import Yoda from "../assets/Yoda.webp"
+import CPO from "../assets/C-3PO.jpg"
+import Obi from "../assets/Obi-Wan.jpg"
 
 const CharacterColumn = styled(Col)`
   margin-top: 30px;
@@ -16,6 +20,18 @@ const Characters = () => (
     <Row>
       <CharacterColumn md="4">
         <Character imageSrc={Annakin} name="Annakin Skywalker" />
+      </CharacterColumn>
+      <CharacterColumn md="4">
+        <Character imageSrc={Jabba} name="Jabba Desilijic Tiure" />
+      </CharacterColumn>
+      <CharacterColumn md="4">
+        <Character imageSrc={Yoda} name="Yoda" />
+      </CharacterColumn>
+      <CharacterColumn md="4">
+        <Character imageSrc={CPO} name="C-3PO" />
+      </CharacterColumn>
+      <CharacterColumn md="4">
+        <Character imageSrc={Obi} name="Obi-Wan Kenobi" />
       </CharacterColumn>
     </Row>
   </Container>
