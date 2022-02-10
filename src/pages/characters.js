a69732c4db7c96e980a1079575a2eb44257a7a93
Anna Sarambelieva <36369561+anisarambelieva@users.noreply.github.com>
2022-02-10 15:17:44 +0200

    Add Characters page (#2)

--- src/pages/characters.js
+++ src/pages/characters.js
@@ -1,7 +1,40 @@
+import { Col, Container, Row } from "react-bootstrap";
+import styled from "styled-components";
+import Character from "../components/character";
+import Header from "../components/header";
+
+import Annakin from "../assets/Annakin.webp"
+import Jabba from "../assets/Jabba.webp"
+import Yoda from "../assets/Yoda.webp"
+import CPO from "../assets/C-3PO.jpg"
+import Obi from "../assets/Obi-Wan.jpg"
+
+const CharacterColumn = styled(Col)`
+  margin-top: 30px;
+`;
+
 const Characters = () => (
-  <div className="Characters">
-    <p>Characters Page</p>
-  </div>
+  <Container>
+    <Header />
+
+    <Row>
+      <CharacterColumn md="4">
+        <Character imageSrc={Annakin} name="Annakin Skywalker" />
+      </CharacterColumn>
+      <CharacterColumn md="4">
+        <Character imageSrc={Jabba} name="Jabba Desilijic Tiure" />
+      </CharacterColumn>
+      <CharacterColumn md="4">
+        <Character imageSrc={Yoda} name="Yoda" />
+      </CharacterColumn>
+      <CharacterColumn md="4">
+        <Character imageSrc={CPO} name="C-3PO" />
+      </CharacterColumn>
+      <CharacterColumn md="4">
+        <Character imageSrc={Obi} name="Obi-Wan Kenobi" />
+      </CharacterColumn>
+    </Row>
+  </Container>
 );
 
 export default Characters;
