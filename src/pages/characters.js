db49c7169c02d5eb4f86afefb2469d2e88d45501
Anna Sarambelieva <anisarambelieva@gmail.com>
2022-02-10 15:09:41 +0200

    Add a single character

--- src/pages/characters.js
+++ src/pages/characters.js
@@ -1,10 +1,23 @@
-import { Container } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
+import styled from "styled-components";
+import Character from "../components/character";
 import Header from "../components/header";
+
+import Annakin from "../assets/Annakin.webp"
+
+const CharacterColumn = styled(Col)`
+  margin-top: 30px;
+`;
 
 const Characters = () => (
   <Container>
     <Header />
 
+    <Row>
+      <CharacterColumn md="4">
+        <Character imageSrc={Annakin} name="Annakin Skywalker" />
+      </CharacterColumn>
+    </Row>
   </Container>
 );
 
