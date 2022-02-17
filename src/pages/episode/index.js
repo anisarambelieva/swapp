1cbc5975d6bc1ed23a664f7ced006c7457e943d3
Anna Sarambelieva <anisarambelieva@gmail.com>
2022-02-17 19:56:50 +0200

    Create episode query

--- src/pages/episode/index.js
+++ src/pages/episode/index.js
@@ -1,3 +1,4 @@
+import { gql } from "@apollo/client";
 import { Col, Container, Row, Image } from "react-bootstrap";
 import styled from "styled-components";
 
@@ -14,84 +15,112 @@ const CharacterColumn = styled(Col)`
   margin-top: 30px;
 `;
 
-const Episode = () => (
-  <Container>
-    <Header />
+const EPISODE_QUERY = gql`
+  query Episode($episodeId: ID!, $perPage: Int, $after: String) {
+    episode(id: $episodeId) {
+      id
+      title
+      openingCrawl
+      image
+      director
+      releaseDate
+      people(first: $perPage, after: $after) {
+        pageInfo {
+          hasNextPage
+          endCursor
+        }
+        edges {
+          node {
+            id
+            name
+            image
+          }
+        }
+      }
+    }
+  }
+`;
 
-    <Row
-      style={{
-        display: "flex",
-        justifyContent: "center",
-      }}
-    >
-      <Col md="9">
-        <Row
-          style={{
-            marginTop: "30px",
-          }}
-        >
-          <Col md="3" style={{ paddingRight: "0" }}>
-            <Image
+const Episode = () => {
+  return (
+    <Container>
+      <Header />
+
+      <Row
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Col md="9">
+          <Row
+            style={{
+              marginTop: "30px",
+            }}
+          >
+            <Col md="3" style={{ paddingRight: "0" }}>
+              <Image
+                style={{
+                  height: "200px",
+                  objectFit: "cover",
+                  width: "100%",
+                  borderRadius: "5px 0px 0px 5px",
+                }}
+                src={ThePhantomMenace}
+              />
+            </Col>
+
+            <Col
               style={{
-                height: "200px",
-                objectFit: "cover",
-                width: "100%",
-                borderRadius: "5px 0px 0px 5px",
+                paddingLeft: "0",
               }}
-              src={ThePhantomMenace}
-            />
-          </Col>
+            >
+              <EpisodeHeader
+                title="Star wars: Episode 1"
+                subtitle="The Phantom Menace"
+              />
+            </Col>
+          </Row>
 
-          <Col
-            style={{
-              paddingLeft: "0",
-            }}
-          >
-            <EpisodeHeader
-              title="Star wars: Episode 1"
-              subtitle="The Phantom Menace"
-            />
-          </Col>
-        </Row>
+          <Row>
+            <Col
+              style={{
+                marginTop: "30px",
+              }}
+            >
+              <Description
+                openingCrawl={episodesData[0].openingCrawl}
+                director="George Lucas"
+                releaseDate="1999-05-19"
+              />
+            </Col>
+          </Row>
 
-        <Row>
-          <Col
-            style={{
-              marginTop: "30px",
-            }}
-          >
-            <Description
-              openingCrawl={episodesData[0].openingCrawl}
-              director="George Lucas"
-              releaseDate="1999-05-19"
-            />
-          </Col>
-        </Row>
+          <Row>
+            {episodesData[0].characters.map(({ id, ...rest }) => (
+              <CharacterColumn key={id} md="4">
+                <Character {...rest} />
+              </CharacterColumn>
+            ))}
+          </Row>
 
-        <Row>
-          {episodesData[0].characters.map(({ id, ...rest }) => (
-            <CharacterColumn key={id} md="4">
-              <Character {...rest} />
-            </CharacterColumn>
-          ))}
-        </Row>
-
-        <Row>
-          <Col
-            style={{
-              marginTop: "30px",
-              display: "flex",
-              justifyContent: "center",
-            }}
-          >
-            <Button $color="#ffe300" $backgroundColor="black">
-              Load More
-            </Button>
-          </Col>
-        </Row>
-      </Col>
-    </Row>
-  </Container>
-);
+          <Row>
+            <Col
+              style={{
+                marginTop: "30px",
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <Button $color="#ffe300" $backgroundColor="black">
+                Load More
+              </Button>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default Episode;
