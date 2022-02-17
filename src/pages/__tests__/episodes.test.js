ed6bcf04fde2a9230e749b69b833cb8f372d0b07
Anna Sarambelieva <anisarambelieva@gmail.com>
2022-02-17 16:12:12 +0200

    Add mocked provider to test

--- src/pages/__tests__/episodes.test.js
+++ src/pages/__tests__/episodes.test.js
@@ -1,10 +1,15 @@
+import { MockedProvider } from "@apollo/client/testing";
 import { render } from "@testing-library/react";
 
 import Episodes from "../episodes.js";
 
 describe("<Episodes/>", () => {
   it("should render Episodes page with correct styles", () => {
-    const { container } = render(<Episodes />);
+    const { container } = render(
+      <MockedProvider>
+        <Episodes />
+      </MockedProvider>
+    );
 
     expect(container).toMatchSnapshot();
   });
