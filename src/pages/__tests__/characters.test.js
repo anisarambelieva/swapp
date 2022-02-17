3d7c302e649b931d973fe63c53c3b9914e9f89fe
Anna Sarambelieva <anisarambelieva@gmail.com>
2022-02-17 15:40:43 +0200

    Add MockedProvider to test

--- src/pages/__tests__/characters.test.js
+++ src/pages/__tests__/characters.test.js
@@ -1,10 +1,15 @@
+import { MockedProvider } from "@apollo/client/testing";
 import { render } from "@testing-library/react";
 
 import Characters from "../characters.js";
 
 describe("<Characters/>", () => {
   it("should render Characters page with correct styles", () => {
-    const { container } = render(<Characters />);
+    const { container } = render(
+      <MockedProvider>
+        <Characters />
+      </MockedProvider>
+    );
 
     expect(container).toMatchSnapshot();
   });
