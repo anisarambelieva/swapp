d19cae9d5ec4b9f589855cc435fac26d04d738e5
Anna Sarambelieva <36369561+anisarambelieva@users.noreply.github.com>
2022-02-24 19:23:12 +0200

    Load data for single character from server (#22)

--- src/pages/character/index.js
+++ src/pages/character/index.js
@@ -1,87 +1,135 @@
+import { gql, useQuery } from "@apollo/client";
 import { Container, Row, Col } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 
-import ThePhantomMenace from "../../assets/The_Phantom_Menace.jpg";
-import Belbullab22Starfighter from "../../assets/starships/Belbullab-22_Starfighter.png";
-import JediInterceptor from "../../assets/starships/Jedi_Interceptor.png";
-import NabooStarSkiff from "../../assets/starships/Naboo_Star_Skiff.jpeg";
-import TradeFederation from "../../assets/starships/Trade_Federation.jpeg";
-import JediStarfighter from "../../assets/starships/Yodas_Jedi_Starfighter.png";
 import Header from "../../components/header.js";
 
 import CharacterCard from "./characterCard.js";
 import Starship from "./starship.js";
 
-const Character = () => (
-  <Container>
-    <Header />
+export const CHARACTER_QUERY = gql`
+  query ($characterId: ID!) {
+    person(id: $characterId) {
+      id
+      name
+      birthYear
+      height
+      mass
+      image
+      homeworld {
+        id
+        name
+      }
+      species {
+        id
+        name
+      }
+      starships(first: 10) {
+        edges {
+          node {
+            id
+            name
+            image
+          }
+        }
+      }
+    }
+  }
+`;
 
-    <Row
-      style={{
-        display: "flex",
-        justifyContent: "center",
-      }}
-    >
-      <Col md="9">
-        <Row style={{ paddingTop: "20px" }}>
-          <Col>
-            <h3
-              style={{
-                textAlign: "center",
-                color: "#4bd5ee",
-                fontWeight: "bold",
-              }}
-            >
-              Obi-Wan Kenobi
-            </h3>
-            <hr />
-          </Col>
-        </Row>
+const Character = () => {
+  const { id } = useParams();
 
-        <Row>
-          <Col>
-            <CharacterCard
-              name="Obi-Wan Kenobi"
-              imageSrc={ThePhantomMenace}
-              height="182"
-              weight="90"
-              species="Human"
-              homeWorld="Stewjon"
-            />
-          </Col>
+  const { data, error, loading } = useQuery(CHARACTER_QUERY, {
+    variables: {
+      characterId: `people.${id}`,
+      perPage: 5,
+    },
+  });
 
-          <Col>
-            <h3
-              style={{
-                textAlign: "center",
-                color: "#3C4858",
-                fontWeight: "bold",
-              }}
-            >
-              Piloted Starships
-            </h3>
+  if (error) {
+    return (
+      <Container>
+        <Header />
+        <div>Something's wrong!</div>
+      </Container>
+    );
+  }
 
-            <hr />
+  if (loading) {
+    return (
+      <Container>
+        <Header />
+        <div>Loading</div>
+      </Container>
+    );
+  }
 
-            <Starship imageSrc={JediStarfighter} name="Jedi starfighter" />
+  const { person } = data;
 
-            <Starship
-              imageSrc={TradeFederation}
-              name="Trade Federation cruiser"
-            />
+  return (
+    <Container>
+      <Header />
 
-            <Starship imageSrc={NabooStarSkiff} name="Naboo star skiff" />
+      <Row
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Col md="9">
+          <Row style={{ paddingTop: "20px" }}>
+            <Col>
+              <h3
+                style={{
+                  textAlign: "center",
+                  color: "#4bd5ee",
+                  fontWeight: "bold",
+                }}
+              >
+                {person.name}
+              </h3>
+              <hr />
+            </Col>
+          </Row>
 
-            <Starship imageSrc={JediInterceptor} name="Jedi Interceptor" />
+          <Row>
+            <Col>
+              <CharacterCard
+                name={person.name}
+                imageSrc={person.image}
+                height={person.height}
+                weight={person.mass}
+                species={person.species ? person.species.name : "Human"}
+                homeWorld={person.homeworld.name}
+              />
+            </Col>
 
-            <Starship
-              imageSrc={Belbullab22Starfighter}
-              name="Belbullab-22 starfighter"
-            />
-          </Col>
-        </Row>
-      </Col>
-    </Row>
-  </Container>
-);
+            <Col>
+              <h3
+                style={{
+                  textAlign: "center",
+                  color: "#3C4858",
+                  fontWeight: "bold",
+                }}
+              >
+                Piloted Starships
+              </h3>
+
+              <hr />
+              {person.starships.edges.map(({ node }) => (
+                <Starship
+                  key={node.id}
+                  imageSrc={node.image}
+                  name={node.name}
+                />
+              ))}
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default Character;
