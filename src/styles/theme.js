ece9c461f638803af7e3f0d82bb7a1ec44a0f191
Anna Sarambelieva <anisarambelieva@gmail.com>
2022-02-28 13:39:19 +0200

    Rename theme file and add light property

--- src/styles/theme.js
+++
@@ -1,8 +0,0 @@
-const theme = {
-  text: {
-    primary: "#4BD5EE",
-    secondary: "#4E5B6E",
-  },
-};
-
-export default theme;
---
+++ src/styles/themes.js
@@ -0,0 +1,10 @@
+const themes = {
+  light: {
+    text: {
+      primary: "#4BD5EE",
+      secondary: "#4E5B6E",
+    },
+  },
+};
+
+export default themes;
