d44a1a73afd34779c0ff922d2ce52dd70006541f
Anna Sarambelieva <anisarambelieva@gmail.com>
2022-02-28 13:31:15 +0200

    Introduce theme to the app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { ApolloProvider } from "@apollo/client";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 
 import client from "./client/index.js";
 import Character from "./pages/character/index.js";
@@ -9,45 +10,47 @@ import Episodes from "./pages/episodes.js";
 import Login from "./pages/login.js";
 import Starship from "./pages/starship.js";
 import ProtectedRoute from "./protectedRoute.js";
-
 import "./styles/font/font.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/App.css";
+import theme from "./styles/theme.js";
 
 const App = () => (
-  <ApolloProvider client={client}>
-    <Router>
-      <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
+  <ThemeProvider theme={theme}>
+    <ApolloProvider client={client}>
+      <Router>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
 
-        <ProtectedRoute path="/episodes">
-          <Episodes />
-        </ProtectedRoute>
+          <ProtectedRoute path="/episodes">
+            <Episodes />
+          </ProtectedRoute>
 
-        <ProtectedRoute path="/episode/:id">
-          <Episode />
-        </ProtectedRoute>
+          <ProtectedRoute path="/episode/:id">
+            <Episode />
+          </ProtectedRoute>
 
-        <ProtectedRoute path="/characters/:id">
-          <Character />
-        </ProtectedRoute>
+          <ProtectedRoute path="/characters/:id">
+            <Character />
+          </ProtectedRoute>
 
-        <ProtectedRoute path="/characters">
-          <Characters />
-        </ProtectedRoute>
+          <ProtectedRoute path="/characters">
+            <Characters />
+          </ProtectedRoute>
 
-        <ProtectedRoute path="/starships/:id">
-          <Starship />
-        </ProtectedRoute>
+          <ProtectedRoute path="/starships/:id">
+            <Starship />
+          </ProtectedRoute>
 
-        <ProtectedRoute path="/">
-          <Episodes />
-        </ProtectedRoute>
-      </Switch>
-    </Router>
-  </ApolloProvider>
+          <ProtectedRoute path="/">
+            <Episodes />
+          </ProtectedRoute>
+        </Switch>
+      </Router>
+    </ApolloProvider>
+  </ThemeProvider>
 );
 
 export default App;
---
+++ src/styles/theme.js
@@ -0,0 +1,8 @@
+const theme = {
+  text: {
+    primary: "#4BD5EE",
+    secondary: "#4E5B6E",
+  },
+};
+
+export default theme;
