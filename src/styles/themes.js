3af95a97b5b714ade0f92428c8d0370c08995999
Anna Sarambelieva <anisarambelieva@gmail.com>
2022-02-28 13:45:54 +0200

    Add light theme button styles

--- src/styles/themes.js
+++ src/styles/themes.js
@@ -4,6 +4,16 @@ const themes = {
       primary: "#4BD5EE",
       secondary: "#4E5B6E",
     },
+    button: {
+      primary: {
+        bg: "#000",
+        text: "#FFE300",
+      },
+      secondary: {
+        bg: "#EFF2F7",
+        text: "#4BD5EE",
+      },
+    },
   },
 };
 
