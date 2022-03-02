791c36a7d54f1555774c57f0e986211d3cddd5cd
Anna Sarambelieva <anisarambelieva@gmail.com>
2022-03-02 14:37:53 +0200

    Add ThemeProvider to Episode header test

--- src/pages/episode/__tests__/header.test.js
+++ src/pages/episode/__tests__/header.test.js
@@ -1,11 +1,15 @@
 import { render } from "@testing-library/react";
+import { ThemeProvider } from "styled-components";
 
+import themes from "../../../styles/themes.js";
 import EpisodeHeader from "../header.js";
 
 describe("<EpisodeHeader />", () => {
   it("should match snapshot", () => {
     const { container } = render(
-      <EpisodeHeader title="Title test" subtitle="Subtitle test" />
+      <ThemeProvider theme={themes["light"]}>
+        <EpisodeHeader title="Title test" subtitle="Subtitle test" />
+      </ThemeProvider>
     );
 
     expect(container).toMatchSnapshot();
