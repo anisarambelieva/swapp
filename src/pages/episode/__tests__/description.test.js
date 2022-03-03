265d0418edf3c8347680424b8a1f3e6901e4bab0
Anna Sarambelieva <anisarambelieva@gmail.com>
2022-03-03 11:22:08 +0200

    Add theme to episode description test

--- src/pages/episode/__tests__/description.test.js
+++ src/pages/episode/__tests__/description.test.js
@@ -1,15 +1,19 @@
 import { render } from "@testing-library/react";
+import { ThemeProvider } from "styled-components";
+import themes from "../../../styles/themes.js";
 
 import Description from "../description.js";
 
 describe("<Description />", () => {
   it("should match snapshot", () => {
     const { container } = render(
-      <Description
-        openingCrawl="openingCrawl test"
-        director="director test"
-        releaseDate="releaseDate test"
-      />
+      <ThemeProvider theme={themes["light"]}>
+        <Description
+          openingCrawl="openingCrawl test"
+          director="director test"
+          releaseDate="releaseDate test"
+        />
+      </ThemeProvider>
     );
 
     expect(container).toMatchSnapshot();
