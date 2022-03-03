d2bf43ded7fd3332ae3c5d944eafafcf064c1b01
Anna Sarambelieva <anisarambelieva@gmail.com>
2022-03-03 11:17:41 +0200

    Add theme to character card test

--- src/pages/character/__tests__/characterCard.test.js
+++ src/pages/character/__tests__/characterCard.test.js
@@ -1,18 +1,22 @@
 import { render } from "@testing-library/react";
+import { ThemeProvider } from "styled-components";
 
+import themes from "../../../styles/themes.js";
 import CharacterCard from "../characterCard.js";
 
 describe("<CharacterCard />", () => {
   it("should match snapshot", () => {
     const { container } = render(
-      <CharacterCard
-        name="testname"
-        imageSrc="test imageSrc"
-        height="test height"
-        weight="test weight"
-        species="test species"
-        homeWorld="test home world"
-      />
+      <ThemeProvider theme={themes["light"]}>
+        <CharacterCard
+          name="testname"
+          imageSrc="test imageSrc"
+          height="test height"
+          weight="test weight"
+          species="test species"
+          homeWorld="test home world"
+        />
+      </ThemeProvider>
     );
 
     expect(container).toMatchSnapshot();
