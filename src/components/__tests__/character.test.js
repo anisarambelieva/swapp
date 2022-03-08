372fb680be1f522223c5c288e91e1b200a0a1a31
Anna Sarambelieva <anisarambelieva@gmail.com>
2022-03-08 12:01:07 +0200

    Add theme provicer to character test and update snapshot

--- src/components/__tests__/character.test.js
+++ src/components/__tests__/character.test.js
@@ -1,11 +1,15 @@
 import { render } from "@testing-library/react";
+import { ThemeProvider } from "styled-components";
+import themes from "../../styles/themes.js";
 
 import Character from "../character.js";
 
 describe("<Character />", () => {
   it("should match snapshot", () => {
     const { container } = render(
-      <Character imageSrc="image" name="Star wars: Episode 1" />
+      <ThemeProvider theme={themes["light"]}>
+        <Character imageSrc="image" name="Star wars: Episode 1" />
+      </ThemeProvider>
     );
 
     expect(container).toMatchSnapshot();
