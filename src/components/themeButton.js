f26bbaec230409e1d3ef1d65e10752e886784bf3
Anna Sarambelieva <anisarambelieva@gmail.com>
2022-03-21 11:19:16 +0200

    Add theme button with moon image

---
+++ src/components/themeButton.js
@@ -0,0 +1,7 @@
+import { Image } from "react-bootstrap";
+
+import Moon from "../assets/Moon.png";
+
+const ThemeButton = () => <Image src={Moon} style={{ height: "30px" }}></Image>;
+
+export default ThemeButton;
